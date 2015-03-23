a97803591e4cd1bab17d5fcfde43ed0e6e223b7c
cohimame <cohimame@gmail.com>
2015-03-23 03:39:30 +0300

    indexing bug now fixed

--- xmlparser.py
+++ xmlparser.py
@@ -6,9 +6,9 @@ wrap         = lambda tag, text: "<{0}>{1}</{0}>".format(tag,text)
 parag_wrap   = lambda parag: wrap("parag",parag)
 sent_wrap    = lambda sent:  wrap("sentence",sent)
 text_wrap    = lambda text:  wrap("text",text)
-token_wrap   = lambda token: wrap("token",token)
-pos_dot_wrap = lambda token: "<token class=\"pos\">{}</token>".format(token)
-neg_dot_wrap = lambda token: "<token class=\"neg\">{}</token>".format(token)
+token_wrap   = lambda token: wrap("t",token)
+pos_dot_wrap = lambda token: "<t class=\"pos\">{}</token>".format(token)
+neg_dot_wrap = lambda token: "<t class=\"neg\">{}</token>".format(token)
 
 
 def read_xml(xml):
@@ -45,15 +45,16 @@ def extract_tokenized_sentences(text):
 def prepare_sentence(tokens):
     aug = ["<sentence>"] + tokens + ["</sentence>"]
     result = ["<sentence>"]
-    for i in range(1,len(tokens)):
+    for i in range(1,len(tokens)+1):
         wrapped = decide(aug[i-1],aug[i],aug[i+1])
         result.append(wrapped)
     result.append("</sentence>")   
     return result
 
 def decide(prev,this,nxt):
+    print(this)
     if this == u'.':
-        if nxt != "</sent>":
+        if nxt != "</sentence>":
             if prev.isdigit() or len(prev) == 1:
                 return pos_dot_wrap(this)
             else:
@@ -67,11 +68,8 @@ def decide(prev,this,nxt):
 def decorate(text):
     txt = []
     for paragraph in text:
-        par = []
-        for sentence in paragraph:
-            sent = ''.join(sentence)
-            par.append(sent)    
-        par = parag_wrap(''.join(par))
+        par = ['\n  '.join(sentence) for sentence in paragraph]    
+        par = parag_wrap('\n'.join(par))
         txt.append(par)
     result = text_wrap('\n'.join(txt))    
     return result
@@ -79,6 +77,7 @@ def decorate(text):
 
 def form_corpora(dest, texts):
     with open(dest, mode = "w" ,encoding='utf-8') as outfile:
+        outfile.write("""<?xml version="1.0" encoding="utf-8" standalone="yes"?>\n""")
         for text in texts:
             parsed_struct = extract_tokenized_sentences(text)
             decorated = decorate(parsed_struct)
@@ -87,9 +86,9 @@ def form_corpora(dest, texts):
 
 tree = read_xml("test10.xml")
 texts = extract_texts(tree)
-form_corpora("output", texts)
+form_corpora("output.xml", texts)
 
-s = ['1','.','пункт','диплома','т','.','т','.']
+s = ['1','.','пункт','диплома','т','.',')','.']
 
 print(prepare_sentence(s))
             
