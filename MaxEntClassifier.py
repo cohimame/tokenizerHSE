50506018cb1b6fd20632cf28351fe182d42aad25
cohimame <cohimame@gmail.com>
2015-03-24 15:14:21 +0300

    meet, maxent classifier!

--- MaxEntClassifier.py
+++ MaxEntClassifier.py
@@ -1,10 +1,27 @@
 from nltk import MaxentClassifier
 from nltk import classify
 import random
+import pickle
+
 
 import CorporaExtractor
 import FeatureExtractor
 
+def load_classifier():
+    f = open('me_classifier.pickle','rb')
+    classifier = pickle.load(f)
+    f.close()
+    return classifier
+
+def save_classifier(classifier):
+    f = open('me_classifier.pickle', 'wb')
+    pickle.dump(classifier, f)
+    f.close()
+    
+def train_classifier(data):
+    me_classifier = MaxentClassifier.train(train_set)
+    return me_classifier
+    
 if __name__ == "__main__":
     """    
     CorporaExtractor.form_train_corpora() # train.xml
@@ -20,7 +37,11 @@ if __name__ == "__main__":
     train_set, test_set = data[:half], data[half:]
 
     
-    me_classifier = MaxentClassifier.train(train_set)
+    #me_classifier = train_classifier(data)
+    #save_classifier(me_classifier)
+    me_classifier = load_classifier()
+    
+    
 
     test_ex = test_set[0][0]
     print(test_ex)
